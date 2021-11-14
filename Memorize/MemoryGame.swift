37379d574b512f27809bf696706d30ffc725db73
Gabriel Carvalho <gabriel315df@gmail.com>
2021-11-13 23:46:57 -0300

    Add initial game logic

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,22 +7,30 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
-    mutating func choose(_ card: Card){
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
-        
-    }
+    private var indexOfChosedCard: Int?
     
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if cards[index].id == card.id {
-                return index
+    mutating func choose(_ card: Card){
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            if let machIndex = indexOfChosedCard {
+                if cards[chosenIndex].content == cards[machIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[machIndex].isMatched = true
+                }
+                indexOfChosedCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfChosedCard = chosenIndex
             }
+            cards[chosenIndex].isFaceUp.toggle()
         }
-        return 0
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
@@ -36,7 +44,7 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
