d00a86db0e7d83b55f1c465dc8ea729ea64e12f4
Gabriel Carvalho <gabriel315df@gmail.com>
2021-11-14 21:38:38 -0300

    Add geometryReader to add content size

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["🚗", "🚎", "🚛", "🚜", "🛵", "🏎", "🚲", "🛴", "✈️", "🚅", "🚀", "🛸", "🚁", "🛶", "🚤", "🚒", "🚐", "⛵️", "🛳", "🚈", "🚠", "🚃"]
+    typealias Card = MemoryGame<String>.Card
+    private static let emojis = ["🚗", "🚎", "🚛", "🚜", "🛵", "🏎", "🚲", "🛴", "✈️", "🚅", "🚀", "🛸", "🚁", "🛶", "🚤", "🚒", "🚐", "⛵️", "🛳", "🚈", "🚠", "🚃"]
     
-    static func createMemoryGame () -> MemoryGame<String> {
+    private static func createMemoryGame () -> MemoryGame<String> {
         MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
                 emojis[pairIndex]
         }
     }
     
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model = createMemoryGame()
     
-    var cards: Array<MemoryGame<String>.Card> {
+    var cards: Array<Card> {
         model.cards
     }
     
     // MARK: - Intent(s)
     
-    func choose(_ card: MemoryGame<String>.Card){
+    func choose(_ card: Card){
         model.choose(card)
     }
     
